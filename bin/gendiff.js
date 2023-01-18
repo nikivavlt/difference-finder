36aca10e3d5923ded6a7ed9f4a859712f71ce89e
Nikita Ivanov <nikivavlt@gmail.com>
2023-01-18 10:42:04 +0200

    Edit gendiff.js, add file1.json and file2.json for tests

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,12 +2,23 @@
 import { Command } from 'commander/esm.mjs';
 const program = new Command();
 
+import fs from 'fs';
+
 
 program.name('gendiff')
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const pathOne = fs.readFileSync(filepath1, 'utf8');
+    const fileOne = JSON.parse(pathOne);
+
+    const pathTwo = fs.readFileSync(filepath2, 'utf8');
+    const fileTwo = JSON.parse(pathTwo);
+
+    console.log(fileOne);
+    console.log(fileTwo);
+  });
 
 program.parse();
-
-const options = program.opts();
-if (options.help) console.log(options);
