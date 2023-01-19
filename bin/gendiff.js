6b53cdf475effc68972a64a5b6bd3c298d9bf95b
Nikita Ivanov <nikivavlt@gmail.com>
2023-01-19 20:18:44 +0200

    Edit node_modules, .gitignore, gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,36 @@
 #!/usr/bin/env node
 import { Command } from 'commander/esm.mjs';
+import fs from 'fs';
+
 const program = new Command();
 
-import fs from 'fs';
+const genDiff = (objectOne, objectTwo) => {
+  const arrayOne = (Object.entries(objectOne)).sort();
+  const arrayTwo = (Object.entries(objectTwo)).sort();
+
+  console.log('{');
+  (Object.keys(objectOne).sort()).map((key) => {
+    console.log(key);
+    console.log(`${key}: ${objectOne.key}`);
+    //if (!objectTwo.hasOwnProperty(key)) return console.log(`- ${key}: ${objectOne.key}`);
+  });
+  console.log('}');
+};
+
+/*arrayOne.map((keyOne) => {
+  console.log(`KEY ONE: ${keyOne}`);
+  if (!objectTwo.hasOwnProperty(keyOne[0])) return console.log(`- ${keyOne[0]}: ${keyOne[1]}`);
+  arrayTwo.map((keyTwo) => {
+    console.log(`COMPARE KEY TWO: ${keyTwo}`);
+    if (keyOne[0] === keyTwo[0] && keyOne[1] === keyTwo[1]) console.log(`  ${keyOne[0]}: ${keyOne[1]}`);
+    else if (keyOne[0] === keyTwo[0] && keyOne[1] !== keyTwo[1]) {
+      console.log(`- ${keyOne[0]}: ${keyOne[1]}`);
+      console.log(`+ ${keyTwo[0]}: ${keyTwo[1]}`);
+      return;
+    }
+    if (!objectOne.hasOwnProperty(keyTwo[0])) console.log(`+ ${keyTwo[0]}: ${keyTwo[1]}`);
+  });
+});*/
 
 
 program.name('gendiff')
@@ -15,10 +43,11 @@ program.name('gendiff')
     const fileOne = JSON.parse(pathOne);
 
     const pathTwo = fs.readFileSync(filepath2, 'utf8');
-    const fileTwo = JSON.parse(pathTwo);
+    const fileTwo = JSON.parse([pathTwo]);
 
-    console.log(fileOne);
-    console.log(fileTwo);
+    genDiff(fileOne, fileTwo);
   });
 
 program.parse();
+
+export default genDiff;
