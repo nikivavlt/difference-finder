d12814012ce2c8a63758108d5cb97af5559abb54
nikita <nikivavlt@gmail.com>
2023-02-11 09:11:17 +0200

    Edit tests, fixtures, json.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,14 +8,26 @@ const dirname = path.dirname(filename);
 
 const getFixturePath = (fileName) => path.join(dirname, '..', '__fixtures__', fileName);
 
-test('genDiff - compare JSON files', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile(getFixturePath('expected_file1.txt')));
+test('genDiff - compare .json files (Format: Stylish)', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile(getFixturePath('expected_stylish')));
 });
 
-test('genDiff - compare YML files', () => {
-  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(readFile(getFixturePath('expected_file1.txt')));
+test('genDiff - compare .yml files (Format: Stylish)', () => {
+  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(readFile(getFixturePath('expected_stylish')));
 });
 
-test('genDiff - compare Nested files', () => {
-  expect(genDiff(getFixturePath('file3.json'), getFixturePath('file4.json'))).toEqual(readFile(getFixturePath('expected_file2.txt')));
+test('genDiff - compare .json files (Format: Plain)', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toEqual(readFile(getFixturePath('expected_plain')));
+});
+
+test('genDiff - compare .yml files (Format: Plain)', () => {
+  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'plain')).toEqual(readFile(getFixturePath('expected_plain')));
+});
+
+test('genDiff - compare .json files (Format: JSON)', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toEqual(readFile(getFixturePath('expected_json')));
+});
+
+test('genDiff - compare .yml files (Format: JSON)', () => {
+  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'json')).toEqual(readFile(getFixturePath('expected_json')));
 });
--- src/formatters/json.js
+++ src/formatters/json.js
@@ -2,9 +2,10 @@ import { isObject } from "../tools.js";
 
 const getValue = (value) => {
   if (isObject(value)) {
-    console.log(value)
-    const { key, val } = value;
-    return `${key}${val}`;
+    const entries = Object
+      .entries(value)
+      .map(([key, val]) => `{${key}:${getValue(val)}}`);
+    return [...entries].join('');
   }
 
   return value;
@@ -13,6 +14,8 @@ const getValue = (value) => {
 const createJSON = (data) => {
   const json = data.map((element) => {
     if (element.type === 'nested') return `{"name":"${element.name}","type":"parent","children":${createJSON(element.value)}}`;
+
+    if (element.type === 'changed') return `{"name":"${element.name}","type":"${element.type}","oldValue":"${getValue(element.oldValue)},"newValue":"${getValue(element.newValue)}"}`;
 
     return `{"name":"${element.name}","type":"${element.type}","value":"${getValue(element.value)}"}`;
   });
